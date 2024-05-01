5e74b3a4cd4e66271707b89e823ff03ca149f6c4
Wolfgang Müller <Wolfgang.Mueller@h-its.org>
2024-05-01 22:06:20 +0200

    first cut

--- GetEntitiesFromSabiork/get_compound_ids_from_files.py
+++ GetEntitiesFromSabiork/get_compound_ids_from_files.py
@@ -1,28 +1,36 @@
 import glob
 import json
 
+from jsonpath_ng import jsonpath, parse
+
 file_names = glob.glob("./JsonEntries/*.json")
 
 
 # turns a json tree into a list of key-value pairs
 def flatten_json( j ):
+    # print(j)
+    jsonpath_expression = parse('sbml.model.listOfSpecies.species[*].@name')
 
-    print("not done, yet")
-    exit()
+    print(jsonpath_expression)
 
-    return list()
+    match = jsonpath_expression.find( j )
+
+    return [m.value for m in match]
+
+
+
+    # return match[0].value
 
 for file_name in file_names:
-
-
-
     f = open(file_name)
 
-    entry_json = print(json.load(f))
+    entry_json = json.load(f)
 
     f.close()
 
     l = flatten_json(entry_json)
+
+    print(l)
 
     exit()
 
